8fa1898e48b6e64fecab87dd00c9415ae21c2e55
CogiArt <72649401+mateo-cogeanu@users.noreply.github.com>
2024-10-03 18:33:37 +0200

    first upload

---
+++ v1.java
@@ -0,0 +1,63 @@
+package first;
+
+public class v1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*String text = "textus maximus";
+		System.out.println(text);
+		int a = 2;
+		int b = 9;
+		System.out.println(a*b);
+		System.out.println(a+b);
+		System.out.println(a/b);
+		System.out.println(a-b);*/
+		
+		/*int x = 9;
+		int y = 2;
+		
+		if(x>y) {
+			System.out.println("X is bigger than Y");
+		}else if(x==y) {
+			System.out.println("X is equal with Y");
+			
+		}else {
+			System.out.println("x is smaller or equal with y");*/
+		/*int a = 6;
+		int b = 8;
+		int c = 3;
+		
+		if (a+b>c) {
+			System.out.println("A+B is bigger than C");
+		}else if(a+b==c) {
+			System.out.println("A+B is equal with C");
+		}else {
+			System.out.println("A+B is samller than C");
+		*/
+			
+		/*int x = 0;
+		
+		while(true) {
+			System.out.println(x);
+			x++;*/
+	
+			
+			int limit = 10;
+			int result = 0;
+			
+			for(int i = 0; i<=limit; i++) {
+				result = result + i;
+				System.out.println(result);
+			
+			
+		
+		
+		
+	
+			
+		}
+			
+		}
+		
+	}	
+
